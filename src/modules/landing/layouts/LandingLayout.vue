2f7dc5ddd360d63d8f45e3ac2fd00bd5f44ea255
Alvarows20 <alvarojpt04.ws@gmail.com>
2024-09-26 23:27:28 -0500

    feat(modules): añadir módulos comunes y pokemons

---
+++ src/modules/landing/layouts/LandingLayout.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <!-- Header -->
+    <header class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8">
+      <!-- Logo a la izquierda -->
+      <div>
+        <a class="flex items-center gap-2 font-semibold" href="/">
+          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
+        </a>
+      </div>
+
+      <!-- Enlaces de navegación a la derecha -->
+      <nav class="flex items-center ml-auto">
+        <div class="flex space-x-6">
+          <RouterLink :to="{ name: 'home' }"> Home </RouterLink>
+          <!--exact-active-class="underline bold-->
+          <!-- Esto es para que en mis links del menu se refleje cada vez que entramos a una sección
+          diferente-->
+          <RouterLink to="/features"></RouterLink>
+          <RouterLink :to="'/pricing'"> Pricing </RouterLink>
+          <RouterLink to="/contact"> Contact </RouterLink>
+        </div>
+
+        <!-- Login separado y alineado a la derecha -->
+        <div class="ml-8">
+          <RouterLink to="/pokemon/1">Pokémons</RouterLink>
+        </div>
+
+        <div class="ml-8">
+          <RouterLink to="/auth"> Login </RouterLink>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Fin Header -->
+
+    <!-- Main -->
+    <main class="flex-1 flex items-center justify-center">
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
+
+      <!--<Router View/>-->
+    </main>
+    <!-- Fin Main -->
+
+    <!-- Footer -->
+    <footer class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8">
+      <p class="flex-1 text-sm text-gray-500 text-center">
+        © {{ new Date().getFullYear() }} Acme Corporation. Derechos reservados
+      </p>
+    </footer>
+    <!-- Fin Footer -->
+  </div>
+</template>
---
+++ src/modules/landing/pages/HomePage.ts
@@ -0,0 +1,50 @@
+import { defineComponent, onActivated, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onDeactivated, onErrorCaptured, onMounted, onRenderTracked, onRenderTriggered, onUnmounted, onUpdated, ref } from "vue";
+
+
+export default defineComponent({
+    setup: ()=>{
+console.log('setup');
+
+const counter = ref(0)
+
+onMounted(()=>{
+    console.log('onMounted');
+});
+onUpdated(()=>{
+    console.log('onUpdated');
+});
+onUnmounted(()=>{
+    console.log('onUnmounted');
+});
+onBeforeMount(()=>{
+    console.log('onBeforeMount');
+});
+onBeforeUpdate(()=>{
+    console.log('onBeforeUpdate');
+});
+onBeforeUnmount(()=>{
+    console.log('onBeforeUnmount');
+});
+onErrorCaptured(()=>{
+    console.log('onErrorCaptured');
+});
+onRenderTracked(()=>{
+    console.log('onRenderTracked');
+});
+onRenderTriggered(()=>{
+    console.log('onRenderTriggered');
+});
+onActivated(()=>{
+    console.log('onActivated');
+});
+onDeactivated(()=>{
+    console.log('onDeactivated');
+});
+
+return{
+   counter,
+}
+
+
+ }
+})
